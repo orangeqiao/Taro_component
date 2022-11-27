f32f9e150584f5badb29053a8dc4144902774f08
chenzhiqiao <13030206192@163.com>
2022-11-27 14:45:32 +0800

    完成验证码框

--- src/comPackages/zzCodeInputPage/index.tsx
+++ src/comPackages/zzCodeInputPage/index.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View,  } from '@tarojs/components'
+import { View, Button,Input } from '@tarojs/components'
 import './index.scss'
 import Taro from '@tarojs/taro'
 import ZzCodeInput from '@/components/ZzCodeInput/ZzCodeInput'
@@ -7,13 +7,15 @@ import ZzCodeInput from '@/components/ZzCodeInput/ZzCodeInput'
 interface IPageOwnProps {
 }
 interface IPageState {
-
+	showType:'number' | 'password'
+	maxLength:number
 }
 export default class Index extends Component<IPageOwnProps, IPageState>  {
 	constructor(props: IPageOwnProps) {
 		super(props)
 		this.state = {
-
+			showType:'number',
+			maxLength:6
 
 		}
 	}
@@ -32,14 +34,26 @@ export default class Index extends Component<IPageOwnProps, IPageState>  {
 	
 
 	}
-
-	
-
+	changeShowType=()=>{
+		this.setState({
+			showType:this.state.showType==='number'?'password':'number'
+		})
+	}
+	changeMaxLength=(event)=>{
+		let {value}=event.detail
+		value = value.substr(0, 1)
+		this.setState({
+			maxLength:value
+		})
+	}
 	render() {
-		let {  } = this.state
+		let { showType,maxLength, } = this.state
 		return (
-			<View className='zz_lr_notice_page'>
-				<ZzCodeInput showType='number' onInput={this.handleInput}/>
+			<View className='zz_code_input_page'>
+				输入验证码长度：
+					<Input className='zz_input' type='digit' value={maxLength+''} onInput={this.changeMaxLength}></Input>
+					<Button className='btn' onClick={this.changeShowType}>明or密</Button>
+				<ZzCodeInput value='' maxLength={maxLength} showType={showType} onInput={this.handleInput}/>
 			</View>
 		)
 	}
--- src/components/ZzCodeInput/ZzCodeInput.tsx
+++ src/components/ZzCodeInput/ZzCodeInput.tsx
@@ -8,19 +8,51 @@ interface IPageOwnProps {
 	'class-name'?: string,
 	customClass?: Partial<Pick<IPageOwnProps, 'class-name'>>
 	showType?: 'number' | 'password',
-	value?: number
+	value?: string
 	onInput?: (event: any, value: string) => void
+	maxLength?: number
 }
 function ZzCodeInput({
 	onInput,
 	customClass,
+	maxLength = 3,
+	showType='password',
+	value,
+	
 }: IPageOwnProps) {
-	const _inputVal = useRef()
-	const [codeList,setCodeList]=useState(['', '', '', '', '', ''] as Array<any>)
-	const [inputVal, setInputVal] = useState()
+	const _inputVal = useRef('' as string)
+	const [codeList, setCodeList] = useState([] as Array<any>)
+	const [inputVal, setInputVal] = useState('' as string)
+	useEffect(() => {
+		codeList.length = maxLength
+		if(value){
+			setInputVal(value)
+			let valueArr:Array<any> = [...value]
+			for (let i = 0; i < codeList.length; i++) {
+				codeList[i] = valueArr[i]
+			}
+		}
+		setCodeList([...codeList])
+
+	}, [maxLength,value])
 	const handleInput = useCallback((event) => {
 		let { value } = event.detail
 		setInputVal(value)
+		_inputVal.current = value
+		if (maxLength !== undefined && _inputVal.current.length > maxLength) {
+			_inputVal.current = _inputVal.current.substr(0, maxLength)
+		}
+		setTimeout(() => {
+			setInputVal(_inputVal.current)
+		}, 0)
+		let valueArr = [...value]
+		for (let i = 0; i < codeList.length; i++) {
+			codeList[i] = valueArr[i]
+		}
+		console.log(codeList, event.detail);
+
+		setCodeList([...codeList])
+
 		onInput && onInput(event, value)
 	}, [onInput])
 
@@ -33,11 +65,13 @@ function ZzCodeInput({
 			<View className='code_list_container'>
 				{
 					codeList.map((e, index) => {
-						return <View key={index} className='list_item'>{e}</View>
+						return <View key={index} className='list_item'>{
+							showType === 'password' ? '●' :
+								e}</View>
 					})
 				}
 			</View>
-			<Input className='zz_input'  type='digit' value={inputVal} onInput={handleInput}></Input>
+			<Input className='zz_input' type='digit' value={inputVal} onInput={handleInput}></Input>
 		</View>
 	)
 
